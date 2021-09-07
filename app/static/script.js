4455b4a9dfeef02efdfe67e57c8f1262243428ef
krishsajeev <88449518+krishsajeev@users.noreply.github.com>
2021-09-07 12:13:49 +0530

    made a booking page for kit

--- app/static/script.js
+++ app/static/script.js
@@ -1,1 +1,19 @@
-
+var app = angular.module("myShoppingList", []); 
+app.controller("myCtrl", function($scope) {
+    $scope.products = ["Milk", "Bread", "Cheese"];
+    $scope.quantity = {"Milk":1,"Bread":1,"Cheese":1};
+    $scope.addItem = function () {
+        $scope.errortext = "";
+        if (!$scope.addMe) {return;}
+        if ($scope.products.indexOf($scope.addMe) == -1) {
+            $scope.products.push($scope.addMe);
+            $scope.quantity[$scope.addMe]=1;
+        } else {
+            $scope.quantity[$scope.addMe]++;
+        }
+    }
+    $scope.removeItem = function (x) {
+        $scope.errortext = "";    
+        $scope.products.splice(x, 1);
+    }
+});
